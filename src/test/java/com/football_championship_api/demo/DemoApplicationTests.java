f9fde5174acf153583fc366b33181f7d07b29e81
Toukoms <tokiniaina090605@gmail.com>
2025-04-30 14:28:27 +0300

    initial commit

---
+++ src/test/java/com/football_championship_api/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football_championship_api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

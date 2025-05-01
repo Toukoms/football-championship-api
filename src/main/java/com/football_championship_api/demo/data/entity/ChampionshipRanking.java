422d8a8c895f922a395382ef6a2c8c79bf90f534
nonieKley <hei.oninaina@gmail.com>
2025-05-01 16:46:01 +0300

    feat(Entity) : add Championship Ranking

---
+++ src/main/java/com/football_championship_api/demo/data/entity/ChampionshipRanking.java
@@ -0,0 +1,7 @@
+package com.football_championship_api.demo.data.entity;
+
+public class ChampionshipRanking {
+    private int rank;
+    private Championship championship;
+    private int differenceGoalsMedian;
+}

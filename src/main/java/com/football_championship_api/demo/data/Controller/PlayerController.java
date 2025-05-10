c81aad31849e212f32edb65f9ad60fdbcc7e47e0
nonieKley <hei.oninaina@gmail.com>
2025-05-10 20:24:16 +0300

    feat : add requestParam

--- src/main/java/com/football_championship_api/demo/data/Controller/PlayerController.java
+++ src/main/java/com/football_championship_api/demo/data/Controller/PlayerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,8 +20,8 @@ public class PlayerController {
     PlayerService service;
 
     @GetMapping
-    public ResponseEntity<List<PlayerRankingDTO>> getBestPlayers(int top, String playingTimeValue){
-        List<PlayerRankingDTO> players = service.getBestPlayers(top,playingTimeValue);
+    public ResponseEntity<List<PlayerRankingDTO>> getBestPlayers(@RequestParam(defaultValue = "5") int top, @RequestParam String playingTimeUnit){
+        List<PlayerRankingDTO> players = service.getBestPlayers(top,playingTimeUnit);
         return ResponseEntity.ok(players);
     }
 
